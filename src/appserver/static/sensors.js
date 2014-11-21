7fd75dcdbd4afc3b5542bc2787b6039196124585
Luke Murphey <Luke@LukeMurphey.net>
2014-11-20 23:12:12 -0600

    Adding javascript file for the sensors view

---
+++ src/appserver/static/sensors.js
@@ -0,0 +1,18 @@
+require.config({
+    paths: {
+    	insteon_sensor_data_cell_renderer: '../app/insteon/js/views/InsteonSensorDataCellRenderer'
+    }
+});
+
+require(['jquery','underscore','splunkjs/mvc', 'insteon_sensor_data_cell_renderer', 'splunkjs/mvc/simplexml/ready!'],
+	function($, _, mvc, InsteonSensorDataCellRenderer){
+	
+	    var leakSensorTable = mvc.Components.get('leak_sensor_table');
+	    
+	    leakSensorTable.getVisualization(function(tableView){
+	        tableView.table.addCellRenderer(new InsteonSensorDataCellRenderer());
+	        tableView.table.render();
+	    });
+	    
+	}
+);
