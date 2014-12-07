fed440d8c2a9a7cc697049a2023064d81b900fe1
Luke Murphey <Luke@LukeMurphey.net>
2014-12-06 22:16:20 -0600

    Added smoke alarms to sensor table

--- src/appserver/static/js/views/InsteonSensorDataCellRenderer.js
+++ src/appserver/static/js/views/InsteonSensorDataCellRenderer.js
@@ -19,9 +19,23 @@ define(function(require, exports, module) {
     		 return $.inArray(cell.field, ["wet", "status", "last_seen"]) >= 0;
 		 },
 		 
+	     defaults: {
+	    	 status_descriptions : { 'clear' : 'All Clear', 'alert' : 'Alert'}
+	     },
+		 
+	     initialize: function() {
+	        	
+	            // Apply the defaults
+	            this.options = _.extend({}, this.defaults, this.options);
+	            
+	            options = this.options || {};
+	            
+	            this.status_descriptions = options.status_descriptions;
+	     },
+		 
 		 render: function($td, cell) {
 			 
-			 // Handle wet state
+			 // Handle problem state (wet)
 			 if(cell.field === "status" && cell.value === "1" || cell.value === 1 || cell.value === "wet"){
 				 $td.html('<span style="display: inline-block; ' + 
 						 'padding-left: 20px;' + 
@@ -31,10 +45,10 @@ define(function(require, exports, module) {
 						 'background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAACXBIWXMAAAsTAAALEwEAmpwYAAAB00lEQVQ4jWP8//8/AzUBEzGK7NYxpFgvZcgiRi0jMS6U9u58/f/vb45nO2r4GRgY/lHkwmMMDPL/fn8T+f/vN88iBgYVQuoJGlgwjSEUxp7Yh2CTayD7812T02GclwcnpDAwMHCQbaBB56+qP9/ewL359/t7Bb2mtw1kGbiBgUH51b7OUnTxN0enFsxiYNDApQ9XLDMqRi/f/+P1DXtskuxCiicfrEiwYsAS41hd6LiDIQ6XYQwMDAw/3903t13NkEKsC9mlfbof/Pv1RQKXgQwMDAxMrFxvnm4tl2dgYPiG14Xelxh8CBnGwMDA8O/3NxHXQwwBGBahCzze+N6EkGEw8HznAzOCBv75+poXnyHccuYbhU0TJzIwMDD8+f6eh6CB/Opqt3AZxsTC8X7lHK88mQCFYwwMDAy8yoY3CRo4O5FhPSMj8090cR4F67VTt1WaGjMwfLjaOrmVgYHpd0M2w1qCBuowMDyW8q5rQBcXNnc7vOoPg5Jc8Iyjf769UZFwqW71ZmC4h64OZ/FlMoGh8tn2xob///+xobnht6Rrbcu5UqZmBgYGDM14y8MNDAzKDe3fEr4+OqHOwMDAwCVjeru4mndhHAMDznAmqoAlBQAAljKnHiVeobMAAAAASUVORK5CYII=);' +
 				 		'">wet</span>');
 				 //background-color: #FFE7CE;
-				 $td.html('<span style="float: right; color: #d85d3c; font-weight: bold"><i style="font-size: 14pt;" class="icon-alert icon-warning"></i> wet</span>');
+				 $td.html('<span style="float: right; color: #d85d3c; font-weight: bold"><i style="font-size: 14pt;" class="icon-alert icon-warning"></i> ' + this.status_descriptions['alert'] + '</span>');
 			 }
 			 
-			 //Handle dry state
+			 //Handle non-problem state (dry)
 			 else if(cell.field === "status" && cell.value === "0" || cell.value === 0 || cell.value === "dry"){
 				 $td.html('<span style="display: inline-block; ' + 
 						 'padding-left: 20px;' + 
@@ -42,7 +56,7 @@ define(function(require, exports, module) {
 						 'height: 20px;' +
 				 		'">dry</span>');
 				 
-				 $td.html('<span style="float: right; color: #65a637; font-weight: bold"><i style="font-size: 14pt;" class="icon-box-checked"></i> dry </span>');
+				 $td.html('<span style="float: right; color: #65a637; font-weight: bold"><i style="font-size: 14pt;" class="icon-box-checked"></i> ' + this.status_descriptions['clear'] + '</span>');
 				 
 			 }
 			 
@@ -66,8 +80,8 @@ define(function(require, exports, module) {
 			 }
 			 
 			 //Handle unknown state
-			 else if(cell.field === "ack" && cell.value === "-1" || cell.value === -1 || cell.value === "nack"){
-				 $td.html('<span class="label label-success">nack</span>');
+			 else if(cell.field === "status" && cell.value === "-1" || cell.value === -1){
+				 $td.html('<span style="float: right; color: #d85d3c; font-weight: bold"><i style="font-size: 14pt;" class="icon-alert icon-warning"></i> Unknown</span>');
 			 }
 			 
 			 // Handle other cases
--- src/appserver/static/sensors.js
+++ src/appserver/static/sensors.js
@@ -10,6 +10,20 @@ require(['jquery','underscore','splunkjs/mvc', 'insteon_sensor_data_cell_rendere
 	    var leakSensorTable = mvc.Components.get('leak_sensor_table');
 	    
 	    leakSensorTable.getVisualization(function(tableView){
+	        tableView.table.addCellRenderer(new InsteonSensorDataCellRenderer(
+	        		{
+	        			'status_descriptions' : {
+	        				'alert' : 'wet',
+	        				'clear' : 'dry'
+	        			}
+	        		}
+	        		));
+	        tableView.table.render();
+	    });
+	    
+	    var smokeSensorTable = mvc.Components.get('smoke_sensor_table');
+	    
+	    smokeSensorTable.getVisualization(function(tableView){
 	        tableView.table.addCellRenderer(new InsteonSensorDataCellRenderer());
 	        tableView.table.render();
 	    });
